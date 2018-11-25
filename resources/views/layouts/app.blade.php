395a14a953d6d860ecf6d3d5c1744f389b231035
Yusufaky <marcosta71999@gmail.com>
2018-11-25 19:54:40 +0000

    Add peça menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,9 @@
                                     <a class="dropdown-item" href="/novosContactos">
                                      {{ __('Novos Contactos') }}
                                   </a>
+                                  <a class="dropdown-item" href="/adicionarpeça">
+                                    {{ __('Adicionar Peças') }}
+                                 </a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                               document.getElementById('logout-form').submit();">
